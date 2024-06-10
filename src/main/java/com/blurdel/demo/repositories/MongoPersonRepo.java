aec5bc9f7b0eae09d2a7d2b9777f2248d04464f1
blurdel <5648737+blurdel@users.noreply.github.com>
2024-06-10 16:12:58 -0700

    debug

--- src/main/java/com/blurdel/demo/repositories/MongoPersonRepo.java
+++ src/main/java/com/blurdel/demo/repositories/MongoPersonRepo.java
@@ -3,7 +3,9 @@ package com.blurdel.demo.repositories;
 import org.springframework.data.repository.CrudRepository;
 
 import com.blurdel.demo.model.Person;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MongoPersonRepo extends CrudRepository<Person, String> {
 
 //	Person findById(String id);
