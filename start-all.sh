64a359e784e719928cfa8e50c6901bb1ce002aed
blurdel <5648737+blurdel@users.noreply.github.com>
2024-07-08 18:52:51 -0700

    Added webapp.yaml

--- start-all.sh
+++ start-all.sh
@@ -2,6 +2,6 @@
 
 kubectl apply -f mysql-config.yaml
 kubectl apply -f mysql-secret.yaml
-# kubectl apply -f pod.yaml
 kubectl apply -f mysql.yaml
+kubectl apply -f webapp.yaml
 kubectl apply -f client-pod.yaml
--- stop-all.sh
+++ stop-all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 kubectl delete -f client-pod.yaml
+kubectl delete -f webapp.yaml
 kubectl delete -f mysql.yaml
-# kubectl delete -f pod.yaml
 kubectl delete -f mysql-secret.yaml
 kubectl delete -f mysql-config.yaml
