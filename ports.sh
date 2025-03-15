f8b222e91c5ced75c49c0888156e4d942123356f
blurdel <5648737+blurdel@users.noreply.github.com>
2025-03-15 13:25:55 -0700

    config updates

--- ports.sh
+++ ports.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 target=${1:-"service/mysql-service"}
+# target="$1"
 
 if [[ -z "${target}" ]]; then
-    "Usage: pf.sh <service/service-name>"
+    "Usage: $0 <service/service-name>"
     exit
 fi
 
 kubectl port-forward  "${target}" 3306:3306 &
-# kubectl port-forward  service/person-webapp-service 8080:8080 &
+
+# ports.sh  service/mysql-service 3306:3306 &
