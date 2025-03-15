c2d214ea7756c8882885c1a071a542fe6dccb860
blurdel <5648737+blurdel@users.noreply.github.com>
2025-03-15 14:05:22 -0700

    Added required names to ports

--- docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mvn clean package -DskipTests
-
-docker build -t basic-rest-controller:1.0.1 .
-
-docker image tag  basic-rest-controller:1.0.1  localhost:5000/basic-rest-controller:1.0.1
-
-docker push  localhost:5000/basic-rest-controller:1.0.1
